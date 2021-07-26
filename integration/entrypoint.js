518fa53735d2336d5ddb013a975d408efbcc36a7
Branden Visser <mrvisser@gmail.com>
2021-07-26 07:18:00 -0400

    Updates

--- integration/entrypoint.js
+++ integration/entrypoint.js
@@ -1,17 +1,12 @@
 void (async () => {
-    const { basePath, host } = (() => {
-        const scripts = document.querySelectorAll('script');
-        for (const script of scripts) {
-            if (script.src.indexOf('?enactive-loader') !== -1 ||
-                script.src.indexOf('ally.js?_=') !== -1) {
-                const a = document.createElement('a');
-                a.href = script.src;
-                const host = `${a.protocol}//${a.hostname}${a.port !== '' ? `:${a.port}` : ''}`;
-                const basePath = a.pathname.split('/').slice(0, -1).join('/');
-                return { basePath, host };
-            }
-        }
-    })();
+    const script = document.querySelector('script[enactive\\.site-id]');
+    if (script === null) {
+        return;
+    }
+    const a = document.createElement('a');
+    a.href = script.src;
+    const host = `${a.protocol}//${a.hostname}`;
+    const basePath = a.pathname.split('/').slice(0, -1).join('/');
     const epDocument = new DOMParser().parseFromString(await fetch(`${host}${basePath}/index.html`).then((r) => r.text()), 'text/html');
     const root = document.createElement('div');
     root.id = 'enactive-app-root';

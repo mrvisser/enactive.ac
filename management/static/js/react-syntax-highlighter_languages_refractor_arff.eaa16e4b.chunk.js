d7c1801af151386cbc51f432bad0b41ae2e702b6
Branden Visser <mrvisser@gmail.com>
2021-08-08 09:22:08 -0400

    Updates

---
+++ management/static/js/react-syntax-highlighter_languages_refractor_arff.eaa16e4b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpenactive-management-ui"]=this["webpackJsonpenactive-management-ui"]||[]).push([[8],{537:function(e,a,n){"use strict";function t(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=t,t.displayName="arff",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.eaa16e4b.chunk.js.map
---
+++ management/static/js/react-syntax-highlighter_languages_refractor_ini.4143b3c6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpenactive-management-ui"]=this["webpackJsonpenactive-management-ui"]||[]).push([[60],{584:function(t,n,e){"use strict";function a(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=a,a.displayName="ini",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.4143b3c6.chunk.js.map

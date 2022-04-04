ca1a2a7c3aa817fa142f33aece4dc2c8be8554b4
Branden Visser <mrvisser@gmail.com>
2022-04-04 12:35:33 -0400

    Updates

--- integration/entrypoint.js
+++ integration/entrypoint.js
@@ -7,11 +7,11 @@ void (async () => {
     a.href = script.src;
     const host = `${a.protocol}//${a.hostname}${a.port === '' ? '' : `:${a.port}`}`;
     const basePath = a.pathname.split('/').slice(0, -1).join('/');
-    const epDocument = new DOMParser().parseFromString(await fetch(`${host}${basePath}/index.html`).then((r) => r.text()), 'text/html');
+    const epDocument = new DOMParser().parseFromString(await fetch(`${host}${basePath}/index.html`).then(r => r.text()), 'text/html');
     const root = document.createElement('div');
     root.id = 'enactive-app-root';
     document.body.appendChild(root);
-    epDocument.head.querySelectorAll('link').forEach((ss) => {
+    epDocument.head.querySelectorAll('link').forEach(ss => {
         var _a;
         const href = (_a = ss.getAttribute('href')) !== null && _a !== void 0 ? _a : '';
         if (ss.rel === 'stylesheet' && href !== '') {
@@ -21,7 +21,7 @@ void (async () => {
             document.head.appendChild(link);
         }
     });
-    epDocument.body.querySelectorAll('script').forEach((s) => {
+    epDocument.head.querySelectorAll('script').forEach(s => {
         const script = document.createElement('script');
         const sSrc = s.getAttribute('src');
         const src = sSrc && sSrc.startsWith('/') ? `${host}${sSrc}` : sSrc;

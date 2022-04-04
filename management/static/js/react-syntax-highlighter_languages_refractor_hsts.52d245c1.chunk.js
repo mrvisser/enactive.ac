a8e3042533a8b5016c84111f88c3875530d22b37
Branden Visser <mrvisser@gmail.com>
2022-04-04 12:35:29 -0400

    Updates

--- management/static/js/react-syntax-highlighter_languages_refractor_hsts.52d245c1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpenactive-management-ui"]=this["webpackJsonpenactive-management-ui"]||[]).push([[55],{575:function(a,e,s){"use strict";function t(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=t,t.displayName="hsts",t.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.52d245c1.chunk.js.map
--- management/static/js/react-syntax-highlighter_languages_refractor_properties.80f1b647.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpenactive-management-ui"]=this["webpackJsonpenactive-management-ui"]||[]).push([[106],{620:function(t,e,n){"use strict";function a(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=a,a.displayName="properties",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.80f1b647.chunk.js.map

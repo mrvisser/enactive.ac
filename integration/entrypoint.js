2268b1c5364bfd382a7fcc738b21c585b9b07997
Branden Visser <mrvisser@gmail.com>
2022-12-13 10:40:02 -0500

    Updates

--- integration/entrypoint.js
+++ integration/entrypoint.js
@@ -1,4 +1,7 @@
 void (async () => {
+    async function getHtml(host, basePath) {
+        return await fetch(`${host}${basePath}/index.html`).then(r => r.text());
+    }
     const script = document.querySelector('script[enactive\\.site-id]');
     if (script === null) {
         return;
@@ -7,7 +10,7 @@ void (async () => {
     a.href = script.src;
     const host = `${a.protocol}//${a.hostname}${a.port === '' ? '' : `:${a.port}`}`;
     const basePath = a.pathname.split('/').slice(0, -1).join('/');
-    const epDocument = new DOMParser().parseFromString(await fetch(`${host}${basePath}/index.html`).then(r => r.text()), 'text/html');
+    const epDocument = new DOMParser().parseFromString(`<html><head><script defer="defer" src="/integration/static/js/main.3e01eb1a.js"></script><link href="/integration/static/css/main.843d04c7.css" rel="stylesheet"></head><body></body></html>`, 'text/html');
     const root = document.createElement('div');
     root.id = 'enactive-app-root';
     document.body.appendChild(root);
